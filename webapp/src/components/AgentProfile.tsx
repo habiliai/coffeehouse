3888775dde7751be55839e9f1523cd305b81bf70
vincent-1014 <vincent.jeong@paust.io>
2025-02-14 05:37:31 +0900

    feat(webapp): refactor AgentProfile component and add ProfileTooltip component

--- webapp/src/components/AgentProfile.tsx
+++ webapp/src/components/AgentProfile.tsx
@@ -1,13 +1,16 @@
 import { Children, isValidElement, PropsWithChildren } from 'react';
-import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
-import { toBase64 } from '@/lib/base64';
-import Image from 'next/image';
+import ProfileImage from './ProfileImage';
+import classNames from 'classnames';
 
 function AgentProfile({
+  className,
+  imageClassName,
   name,
   imageUrl,
   children,
 }: PropsWithChildren<{
+  className?: string;
+  imageClassName?: string;
   name: string;
   imageUrl: string;
 }>) {
@@ -19,49 +22,32 @@ function AgentProfile({
     return isValidElement(child) && child.type === AgentProfile.Icon;
   });
 
+  const tooltip = Children.toArray(children).find((child) => {
+    return isValidElement(child) && child.type === AgentProfile.Tooltip;
+  });
+
   return (
-    <div className="flex w-32 flex-col items-center gap-y-2 p-2">
-      <div className="relative size-16 overflow-hidden rounded-full">
-        <AgentImage alt={`agent-${name}`} src={imageUrl} />
-        {icon && <div className="absolute bottom-0 right-0">{icon}</div>}
+    <div className={className}>
+      <div className="group relative flex flex-col items-center gap-y-2">
+        <div className={classNames('relative', imageClassName)}>
+          <ProfileImage
+            className="rounded-full"
+            alt={`agent-${name}`}
+            src={imageUrl}
+          />
+          {icon && <div className="absolute bottom-0 right-0">{icon}</div>}
+        </div>
+        {label}
+
+        {tooltip && (
+          <div className="pointer-events-none absolute bottom-full left-1/2 z-50 mb-2 flex w-52 -translate-x-1/2 rounded-lg bg-gray-700 p-2 opacity-0 ring-1 ring-inset ring-gray-400 transition-opacity group-hover:opacity-100">
+            {tooltip}
+          </div>
+        )}
       </div>
-      {label}
     </div>
   );
 }
-
-function shimmer(w: number | string, h: number | string) {
-  return `<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
-}
-
-const placeholder: PlaceholderValue = `data:image/svg+xml;base64,${toBase64(shimmer('100%', '100%'))}`;
-
-const AgentImage = ({ alt, src }: { alt: string; src: string }) => {
-  src = src ?? placeholder;
-  if (src === '') src = placeholder;
-
-  return (
-    <Image
-      alt={alt}
-      src={src}
-      width={64}
-      height={64}
-      sizes="100vw"
-      placeholder={placeholder}
-    />
-  );
-};
 
 const AgentLabel = ({
   className,
@@ -78,5 +64,13 @@ const AgentIcon = ({
   return <div className={className}>{children}</div>;
 };
 AgentProfile.Icon = AgentIcon;
+
+const AgentTooltip = ({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) => {
+  return <div className={className}>{children}</div>;
+};
+AgentProfile.Tooltip = AgentTooltip;
 
 export default AgentProfile;
