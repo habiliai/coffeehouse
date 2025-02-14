70b3b184aaf9d570af5acd2e3db56e8d84ad97c7
jcooky <dennis@habili.ai>
2025-02-14 20:27:15 +0900

    fix(api): add domain for testing into next.config.ts

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.logo.dev',
+        pathname: '/**',
+      }
+    ]
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
