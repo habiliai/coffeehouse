6ca5502364d98ed3882ca6101c9cf32ebbf85772
jcooky <dennis@habili.ai>
2025-02-19 15:21:05 +0900

    fix(api): add key into compoment UserChatBubble

--- webapp/src/app/workflow/UserChatBubble.tsx
+++ webapp/src/app/workflow/UserChatBubble.tsx
@@ -2,28 +2,31 @@ import { Fragment } from 'react';
 
 export default function UserChatBubble({
   text,
+  id,
 }: {
+  id: string;
   text: string;
-  mentions: string[];
 }) {
   return (
     <div className="flex flex-row-reverse">
-      <div className="flex justify-start items-center max-w-[75%] rounded-md bg-[#f3f3f3] p-4 text-sm text-gray-900 gap-y-0.5 gap-x-1 flex-wrap">
-        {text.split('\n').map((line) => (
-          <>
-            {line.split(' ').map((word) => (
-              <>
-                {word.startsWith('@') ? (
-                  <span className="font-semibold text-blue-500 underline">
+      <div className="flex max-w-[75%] flex-wrap items-center justify-start gap-x-1 gap-y-0.5 rounded-md bg-[#f3f3f3] p-4 text-sm text-gray-900">
+        {text.split('\n').map((line, i) => (
+          <Fragment key={`user-messsage-${id}-line-${i}`}>
+            {line
+              .split(' ')
+              .map((word, j) => {
+                const key = `user-message-${id}-line-${i}-word-${j}`;
+                return word.startsWith('@') ? (
+                  <span key={key}
+                        className="font-semibold text-blue-500 underline">
                     {word}
                   </span>
                 ) : (
-                  <span>{word}</span>
-                )}
-              </>
-            ))}
+                  <span key={key}>{word}</span>
+                );
+              })}
             <br />
-          </>
+          </Fragment>
         ))}
       </div>
     </div>
